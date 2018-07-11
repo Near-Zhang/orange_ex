997fb58dd1f67822600987b92bb48275a1b7452f
Near-Zhang <704220802@qq.com>
2018-07-11 18:25:23 +0800

    repair conf/ and Makefile

--- install/orange-v0.6.4_ex.sql
+++ install/orange-v0.6.4_ex.sql
@@ -313,6 +313,23 @@ INSERT INTO `waf` (`id`, `key`, `value`, `type`, `op_time`)
 VALUES
     (1,'1','{}','meta','2016-11-11 11:11:11');
 
+
+# Dump of table upstream
+# ------------------------------------------------------------
+DROP TABLE IF EXISTS `upstream`;
+
+CREATE TABLE `upstream` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(2000) NOT NULL DEFAULT '',
+  `type` varchar(11) DEFAULT '0',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
 /*!40000 ALTER TABLE `waf` ENABLE KEYS */;
 UNLOCK TABLES;
 
